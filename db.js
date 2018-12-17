723994d6fcb7b6dbe0970022a74814271ddcd320
lazzyguy <igenjarus@gmail.com>
2018-12-17 16:08:23 +0530

    init husky

--- db.js
+++ db.js
@@ -1,2 +1,7 @@
-const mongoose = require('mongoose');
-mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds125392.mlab.com:25392/lekhachitr`, { useNewUrlParser: true });
+const mongoose = require('mongoose')
+mongoose.connect(
+	`mongodb://${process.env.DB_USER}:${
+		process.env.DB_PASS
+	}@ds125392.mlab.com:25392/lekhachitr`,
+	{ useNewUrlParser: true }
+)
